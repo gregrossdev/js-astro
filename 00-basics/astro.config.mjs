5fafc791b7ae318af5871dd04aa0ede864de180c
Gregory Ross <gregross.dev@gmail.com>
2022-10-31 17:17:07 -0400

    add mdx

--- 00-basics/astro.config.mjs
+++ 00-basics/astro.config.mjs
@@ -1,8 +1,12 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
+import mdx from "@astrojs/mdx";
+
+// https://astro.build/config
 export default defineConfig({
   markdown: {
-    drafts: true,
+    drafts: true
   },
+  integrations: [mdx()]
 });
