21f9ad18c2207daeed43faf2513af0457ab2a35f
Gregory Ross <gregross.dev@gmail.com>
2022-11-10 15:50:32 -0500

    add mdx

--- 02-practice/calendar-entries/astro.config.mjs
+++ 02-practice/calendar-entries/astro.config.mjs
@@ -9,6 +9,8 @@ import tailwind from "@astrojs/tailwind";
 import svelte from "@astrojs/svelte";
 
 // https://astro.build/config
+
+// https://astro.build/config
 export default defineConfig({
   site: 'https://example.com',
   integrations: [mdx(), sitemap(), tailwind(), svelte()]
