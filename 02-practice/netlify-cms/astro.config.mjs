d7769030a97af7c999efe7adce7198ee60c827cf
Gregory Ross <gregross.dev@gmail.com>
2022-12-21 15:09:39 -0500

    netlify cms

--- 02-practice/netlify-cms/astro.config.mjs
+++ 02-practice/netlify-cms/astro.config.mjs
@@ -1,10 +1,45 @@
 import { defineConfig } from 'astro/config';
-
-// https://astro.build/config
 import tailwind from "@astrojs/tailwind";
+import NetlifyCMS from 'astro-netlify-cms';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://jimmy-smith.netlify.app', 
-  integrations: [tailwind()]
+  integrations: [
+    tailwind(),
+    NetlifyCMS({
+      config: {
+        backend: {
+          name: 'git-gateway',
+          branch: 'main',
+        },
+        collections: [
+          {
+            label: 'January',
+            name: 'january',
+            folder: 'src/pages/date/01',
+            create: true,
+            delete: true,
+            fields: [
+              { label: 'Date',  name: 'date', widget: 'datetime',  date_format: "MM.DD", time_format: false },
+              { label: 'Title', name: 'title', widget: 'string'  },
+              { label: 'Body' , name: 'body', widget: 'markdown'},
+            ],
+          },
+          {
+            label: 'February',
+            name: 'february',
+            folder: 'src/pages/date/02',
+            create: true,
+            delete: true,
+            fields: [
+              { label: 'Date',  name: 'date', widget: 'datetime',  date_format: "MM.DD", time_format: false },
+              { label: 'Title', name: 'title', widget: 'string'  },
+              { label: 'Body' , name: 'body', widget: 'markdown'},
+            ],
+          },
+        ],
+      },
+    }),
+  ]
 });
